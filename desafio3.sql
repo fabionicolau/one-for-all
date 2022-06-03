6c02d05a8afc76783ca231d7534e5c4808a39b15
fabionicolau <fabionicolaujr@gmail.com>
2022-06-02 22:32:00 -0300

    requisito 4 - ok

--- desafio3.sql
+++ desafio3.sql
@@ -5,7 +5,11 @@ ROUND(sum(musicas.duracao_segundos / 60), 2) as total_minutos
 
 FROM
 SpotifyClone.usuario as usuario
-INNER JOIN SpotifyClone.historico AS historico ON historico.usuario_id = usuario.usuario_id
-INNER JOIN SpotifyClone.musicas AS musicas ON historico.musicas_id = musicas.musicas_id
+INNER JOIN SpotifyClone.historico AS historico 
+ON historico.usuario_id = usuario.usuario_id
+INNER JOIN SpotifyClone.musicas AS musicas 
+ON historico.musicas_id = musicas.musicas_id
 
-GROUP BY usuario;
+GROUP BY usuario
+
+ORDER BY usuario ASC;
